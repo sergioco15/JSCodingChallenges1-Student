2019cbe9f0f869413ee5217d5b55459cf29419a7
sergioco15 <cainvela15@gmail.com>
2022-07-20 12:54:08 -0400

    js ch S1

--- Challenge01/site01.js
+++ Challenge01/site01.js
@@ -14,12 +14,13 @@ const marvelHeroes = [
     "Spider-Man",
     "Thor",
     "Wasp"
-]
+];
+
 //driver function used for display and passing values.
 function findHero() {
 
     //implement the function findLongestString that returns the longest word.
-    let lword = findLongestString(marvelHeroes);
+    let lword = findLongestStringB(marvelHeroes);
 
     //used for display. no need to change
     document.getElementById("results").innerHTML = lword;
@@ -31,6 +32,30 @@ function findHero() {
 //takes an array of strings and returns the longest one. 
 function findLongestString(namesArry) {
 
-    return "";
+    let lstring = "";
+    
+    for (let index = 0; index < namesArry.length; index++) {
+         
+        
+       if (namesArry[index].length > lstring.length){
+        lstring = namesArry[index];
+       }
+        
+    }
 
+    return lstring;
+
+}
+
+function findLongestStringB(namesArray){
+    let lstring = "";
+
+    namesArray.forEach(name => {
+         if (name.lenght > lstring.lenght){
+            lstring = name;
+         }
+
+    });
+
+    return lstring;
 }
--- Challenge08/site08.js
+++ Challenge08/site08.js
@@ -2,46 +2,75 @@ const numbers = [9, 1, 3, 27, 15, -5, 13, 7];
 
 //driver function used for display and passing values.
 function displayNumbers() {
-    
-    //implement the four functions to return sum, avg, max and min
-    let sum = sumNumbers(numbers);
-    let avg = avgNumber(numbers);
-    let max = maxNumber(numbers);
-    let min = minNumber(numbers);
-    let sumRec = sumNumbersRecursive(numbers,index);
+  //implement the four functions to return sum, avg, max and min
+  let sum = sumNumbers(numbers);
+  let avg = avgNumber(numbers);
+  let max = maxNumber(numbers);
+  let min = minNumber(numbers);
+  //let sumRec = sumNumbersRecursive(numbers,index);
 
-    //used for display not need to change unless you are doing only the sum function
-    let msg = `Sum = ${sum}<br>Avg = ${avg.toFixed(2)}<br>Max = ${max}<br>Min = ${min}`;
-    
-    //display the message    
-    document.getElementById("results").innerHTML = msg;
-    document.getElementById("numberslist").innerHTML = numbers.toString();
+  //used for display not need to change unless you are doing only the sum function
+  let msg = `Sum = ${sum}<br>Avg = ${avg.toFixed(
+    2
+  )}<br>Max = ${max}<br>Min = ${min}`;
+  let msg2 = `Sum = ${sum}`;
+
+  //display the message
+  document.getElementById("results").innerHTML = msg;
+  document.getElementById("numberslist").innerHTML = numbers.toString();
 }
 
 //takes an array of numbers and returns the sum of all the numbers
 function sumNumbers(numArry) {
-    return 0;
+  let sum = 0;
+
+  for (let index = 0; index < numArray.length; index++) {
+    sum += numArray[index];
+  }
+
+  return sum;
 }
 
 //takes an array of numbers and the current index in the array and returns the sum of all the numbers using recursion
-//Recursion is the technique of the function calling itself. 
-//NOTE * SEE CHALLENGE 11. 
+//Recursion is the technique of the function calling itself.
+//NOTE * SEE CHALLENGE 11.
 function sumNumbersRecursive(numArry, index) {
-    return 0;
+  return 0;
 }
-
 
 //takes an array of numbers and returns the average of all the numbers
 function avgNumber(numArry) {
-    return 0;
+  let avg = 0;
+  let sum = 0;
+
+  for (let index = 0; index < numArray.length; index++) {
+    sum += numArray[index];
+  }
+
+  avh = sum / numArry.length;
+
+  return avg;
 }
 
 //takes an array of numbers and returns the max number
 function maxNumber(numArry) {
-    return 0;
+    let maxNumber = 0;
+
+
+    for (let index = 0; index < numArray.length; index++) {
+
+        if(maxNumber> numArry[index]){
+        maxNumber = numArray[index];
+        }
+        
+    }
+
+
+
+  return 0;
 }
 
 //takes an array of numbers and returns the min number
 function minNumber(numArry) {
-    return 0;
+  return 0;
 }
